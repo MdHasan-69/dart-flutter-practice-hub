12d71530b70b53a98cdb58c4d7141278fbe14882
Md Hasan <212467026+MdHasan-69@users.noreply.github.com>
2026-05-05 22:20:58 +0600

    Add basic Dart print practice

---
+++ dart_foundation_console/lib/module_01_dart_basics/class_01_setup_first_steps/basic_print_practice.dart
@@ -0,0 +1,15 @@
+import 'dart:collection';
+
+main(){
+  String name = 'Hasan';
+  int age = 23;
+  double cg = 3.5;
+  bool who = true;
+  String check = who ? "am" : "am not"; 
+
+  print("Hey!!! I $check $name.");
+  print("I am $age years old.");
+  print("My unversity CG was $cg.");
+  print("I love Flowers.");
+
+}
