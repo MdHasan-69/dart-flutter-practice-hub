e7ff28d9b99322092708d9df1d3457f8f19d22a3
Md Hasan <212467026+hasancodecraft@users.noreply.github.com>
2026-05-15 23:58:10 +0600

    Start Dart for and while loop examples

---
+++ dart_foundation_console/lib/module_03_control_flow/class_02_loops/loops.dart
@@ -0,0 +1,61 @@
+void main(){
+  List<int> numbers = [10,20,30,40,50];
+
+  //Basic for loop
+  for(int i = 0; i <numbers.length; i++){
+    print("Number[${i+1}]: ${numbers[i]}");
+  }
+
+  // Calculate grades
+  List<int> markList = [85, 90, 78, 92, 88];
+  List<String> gradeList = [];
+
+  for (int mark in markList){
+    String temporaryGrade;
+
+    if(mark >= 90){
+      temporaryGrade = 'A';
+      } else if (mark >= 80){
+        temporaryGrade = 'B';
+      } else if (mark >= 70){
+        temporaryGrade = 'C';
+      } else if (mark >= 60){
+        temporaryGrade = 'D';
+      } else{
+        temporaryGrade = 'F';
+      }
+
+      print("Marks: $mark, Grade: $temporaryGrade");
+      gradeList.add(temporaryGrade);
+      }
+
+  print('Grades: $gradeList');
+
+  //While loop
+
+ // While loop is useful when we don't know the exact number of repetitions
+  /* while(condition){
+    // code to be executed
+    // update condition
+    } */
+
+   int batteryLevel = 5;
+   bool isCharging = false;
+   
+   while(batteryLevel > 0){
+      print('Battery: $batteryLevel% - Still running...');
+
+      if(batteryLevel == 3 && !isCharging){
+        print("Battery low. Starting to charge...");
+        batteryLevel += 4;
+        isCharging = true;
+      }
+      else if(batteryLevel == 1 ){
+      print('Battery: 0% - Shutting down...');
+      }
+
+    batteryLevel--;
+   }
+   
+  print("Goodbye");
+  }
